04763a8b2938b10f489e6ab053a9deb1f4bd3c2e
margao <matthew.argao@tomiaglobal.com>
2019-07-11 01:36:22 -0400

    Added Chart Range Filter functionality.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -9,70 +9,12 @@
         xs10
         offset-xs1
       >
-
-          <GChart
-            :settings="{ packages: ['timeline'] }"
-            type="Timeline"
-            :data="chartData"
-            :options="chartOptions"
-          />
+      <div id="dashboard" style="width:1200px;">
+          <div id="chart" style="position: relative; width: 1200px; height: 600px; backgroundColor: #ffffff;"></div>
+          <div id="control"></div>
+      </div>
+      <div id="junk_div" style="display: none;"></div>
     </v-flex>
-      <!--
-      <v-flex
-        md12
-        sm12
-        lg4
-      >
-
-
-        <material-chart-card
-          :data="emailsSubscriptionChart.data"
-          :options="emailsSubscriptionChart.options"
-          :responsive-options="emailsSubscriptionChart.responsiveOptions"
-          color="red"
-          type="Bar"
-        >
-          <h4 class="title font-weight-light">Email Subscription</h4>
-          <p class="category d-inline-flex font-weight-light">Last Campaign Performance</p>
-
-          <template slot="actions">
-            <v-icon
-              class="mr-2"
-              small
-            >
-              mdi-clock-outline
-            </v-icon>
-            <span class="caption grey--text font-weight-light">updated 10 minutes ago</span>
-          </template>
-        </material-chart-card>
-      </v-flex>
-
-      <v-flex
-        md12
-        sm12
-        lg4
-      >
-        <material-chart-card
-          :data="dataCompletedTasksChart.data"
-          :options="dataCompletedTasksChart.options"
-          color="green"
-          type="Line"
-        >
-          <h3 class="title font-weight-light">Completed Tasks</h3>
-          <p class="category d-inline-flex font-weight-light">Last Last Campaign Performance</p>
-
-          <template slot="actions">
-            <v-icon
-              class="mr-2"
-              small
-            >
-              mdi-clock-outline
-            </v-icon>
-            <span class="caption grey--text font-weight-light">campaign sent 26 minutes ago</span>
-          </template>
-        </material-chart-card>
-      </v-flex>
-      -->
       <v-flex
         sm6
         xs12
@@ -121,225 +63,6 @@
           sub-text="Issues needing attention."
         />
       </v-flex>
-      <!--
-      <v-flex
-        sm6
-        xs12
-        md6
-        lg3
-      >
-        <material-stats-card
-          color="info"
-          icon="mdi-twitter"
-          title="Followers"
-          value="+245"
-          sub-icon="mdi-update"
-          sub-text="Just Updated"
-        />
-      </v-flex>
-      <v-flex
-        md12
-        lg6
-      >
-        <material-card
-          color="orange"
-          title="Employee Stats"
-          text="New employees on 15th September, 2016"
-        >
-          <v-data-table
-            :headers="headers"
-            :items="items"
-            hide-actions
-          >
-            <template
-              slot="headerCell"
-              slot-scope="{ header }"
-            >
-              <span
-                class="font-weight-light text-warning text--darken-3"
-                v-text="header.text"
-              />
-            </template>
-            <template
-              slot="items"
-              slot-scope="{ index, item }"
-            >
-              <td>{{ index + 1 }}</td>
-              <td>{{ item.name }}</td>
-              <td class="text-xs-right">{{ item.salary }}</td>
-              <td class="text-xs-right">{{ item.country }}</td>
-              <td class="text-xs-right">{{ item.city }}</td>
-            </template>
-          </v-data-table>
-        </material-card>
-      </v-flex>
-      <v-flex
-        md12
-        lg6
-      >
-        <material-card
-          class="card-tabs"
-          color="green">
-          <v-flex
-            slot="header"
-          >
-            <v-tabs
-              v-model="tabs"
-              color="transparent"
-              slider-color="white"
-            >
-              <span
-                class="subheading font-weight-light mr-3"
-                style="align-self: center"
-              >Tasks:</span>
-              <v-tab class="mr-3">
-                <v-icon class="mr-2">mdi-bug</v-icon>
-                Bugs
-              </v-tab>
-              <v-tab class="mr-3">
-                <v-icon class="mr-2">mdi-code-tags</v-icon>
-                Website
-              </v-tab>
-              <v-tab>
-                <v-icon class="mr-2">mdi-cloud</v-icon>
-                Server
-              </v-tab>
-            </v-tabs>
-          </v-flex>
-
-          <v-tabs-items v-model="tabs">
-            <v-tab-item
-              v-for="n in 3"
-              :key="n"
-            >
-              <v-list three-line>
-                <v-list-tile @click="complete(0)">
-                  <v-list-tile-action>
-                    <v-checkbox
-                      :value="list[0]"
-                      color="green"
-                    />
-                  </v-list-tile-action>
-                  <v-list-tile-title>
-                    Sign contract for "What are conference organized afraid of?"
-                  </v-list-tile-title>
-                  <div class="d-flex">
-                    <v-tooltip
-                      top
-                      content-class="top">
-                      <v-btn
-                        slot="activator"
-                        class="v-btn--simple"
-                        color="success"
-                        icon
-                      >
-                        <v-icon color="primary">mdi-pencil</v-icon>
-                      </v-btn>
-                      <span>Edit</span>
-                    </v-tooltip>
-                    <v-tooltip
-                      top
-                      content-class="top">
-                      <v-btn
-                        slot="activator"
-                        class="v-btn--simple"
-                        color="danger"
-                        icon
-                      >
-                        <v-icon color="error">mdi-close</v-icon>
-                      </v-btn>
-                      <span>Close</span>
-                    </v-tooltip>
-
-                  </div>
-                </v-list-tile>
-                <v-divider/>
-                <v-list-tile @click="complete(1)">
-                  <v-list-tile-action>
-                    <v-checkbox
-                      :value="list[1]"
-                      color="success"
-                    />
-                  </v-list-tile-action>
-                  <v-list-tile-title>
-                    Lines From Great Russian Literature? Or E-mails From My Boss?
-                  </v-list-tile-title>
-                  <div class="d-flex">
-                    <v-tooltip
-                      top
-                      content-class="top">
-                      <v-btn
-                        slot="activator"
-                        class="v-btn--simple"
-                        color="success"
-                        icon
-                      >
-                        <v-icon color="primary">mdi-pencil</v-icon>
-                      </v-btn>
-                      <span>Edit</span>
-                    </v-tooltip>
-
-                    <v-tooltip
-                      top
-                      content-class="top">
-                      <v-btn
-                        slot="activator"
-                        class="v-btn--simple"
-                        color="danger"
-                        icon>
-                        <v-icon color="error">mdi-close</v-icon>
-                      </v-btn>
-                      <span>Close</span>
-                    </v-tooltip>
-                  </div>
-                </v-list-tile>
-                <v-divider/>
-                <v-list-tile @click="complete(2)">
-                  <v-list-tile-action>
-                    <v-checkbox
-                      :value="list[2]"
-                      color="success"
-                    />
-                  </v-list-tile-action>
-                  <v-list-tile-title>
-                    Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit
-                  </v-list-tile-title>
-                  <div class="d-flex">
-                    <v-tooltip
-                      top
-                      content-class="top">
-                      <v-btn
-                        slot="activator"
-                        class="v-btn--simple"
-                        color="success"
-                        icon
-                      >
-                        <v-icon color="primary">mdi-pencil</v-icon>
-                      </v-btn>
-                      <span>Edit</span>
-                    </v-tooltip>
-                    <v-tooltip
-                      top
-                      content-class="top">
-                      <v-btn
-                        slot="activator"
-                        class="v-btn--simple"
-                        color="danger"
-                        icon
-                      >
-                        <v-icon color="error">mdi-close</v-icon>
-                      </v-btn>
-                      <span>Close</span>
-                    </v-tooltip>
-
-                  </div>
-                </v-list-tile>
-              </v-list>
-            </v-tab-item>
-          </v-tabs-items>
-        </material-card>
-      </v-flex>
-    -->
     </v-layout>
   </v-container>
 </template>
@@ -353,11 +76,6 @@ export default {
     return {
       chartData: [],
       chartOptions: {
-        chart: {
-          title: 'Company Performance',
-          subtitle: 'Sales, Expenses, and Profit: 2014-2017',
-        },
-        height: 500,
         hAxis: {
           minValue: '',
           maxValue: ''
@@ -374,7 +92,79 @@ export default {
       }
     }
   },
+  mounted: function() {
+    console.log("hello");
+    document.onreadystatechange = () => {
+  if (document.readyState == "complete") {
+      // run code here
+      this.drawDashboard();
+  }
+}
+  },
   methods: {
+    drawDashboard() {
+
+                var control = new google.visualization.ControlWrapper({
+    controlType: 'ChartRangeFilter',
+    containerId: 'control',
+    options: {
+        // Filter by the date axis.
+        filterColumnIndex: 1,
+        ui: {
+            chartType: 'LineChart',
+            minRangeSize: 600000,
+            chartOptions: {
+                width: 1200,
+                height: 70,
+                hAxis: {
+                    format: 'hh:mm:ss',
+                    ticks: [
+                        this.chartOptions.hAxis.minValue,
+
+                        this.chartOptions.hAxis.maxValue
+                    ]
+                },
+                chartArea: {
+                  width: '90%', // make sure this is the same for the chart and control so the axes align right
+                  height: '90%'
+                }
+            },
+            chartView: {
+                columns: [1, {
+                    type: 'number',
+                    calc: function () {return 0;}
+                }]
+            }
+        }
+    }
+});
+
+var dashboard = new google.visualization.Dashboard(
+    document.getElementById('dashboard'));
+
+                var chart = new google.visualization.ChartWrapper({
+                    'chartType': 'Timeline',
+                        'containerId': 'chart',
+                        'options': {
+                        'width': 1200,
+                            'height': 600,
+                            'chartArea': {
+                            width: '100%', // make sure this is the same for the chart and control so the axes align right
+                            height: '100%'
+                        },
+                            'backgroundColor': '#ffd'
+                    },
+                        'view': {
+                        'columns': [0, 1, 2]
+                    }
+
+                });
+
+                var data = new google.visualization.arrayToDataTable(this.chartData);
+
+                dashboard.bind(control, chart);
+                dashboard.draw(data);
+            },
     refactorProcess() {
       console.log('in refactor')
       var tempProcess = {}
@@ -424,13 +214,11 @@ export default {
         }
         if (minProcessBeginArray.length > 0) {
           this.registryData[registryID].BeginTime = Math.min(...minProcessBeginArray)
-          console.log(registryID + ' minarray ' + minProcessBeginArray)
-          console.log(Math.min(...minProcessBeginArray))
+
         }
         if (maxProcessEndArray.length > 0) {
           this.registryData[registryID].EndTime = Math.max(...maxProcessEndArray)
-          console.log(registryID + ' maxarray ' + maxProcessEndArray)
-          console.log(Math.min(...maxProcessEndArray))
+
         }
 
         maxProcessEndArray = []
@@ -447,8 +235,8 @@ export default {
         chartMax.push(this.registryData[i].EndTime)
       }
 
-      this.chartOptions.hAxis.minValue = new Date(Math.min(...chartMin) - 10000)
-      this.chartOptions.hAxis.maxValue = new Date(Math.max(...chartMax) + 10000)
+      this.chartOptions.hAxis.minValue = new Date(Math.min(...chartMin))
+      this.chartOptions.hAxis.maxValue = new Date(Math.max(...chartMax))
 
     }
   },
@@ -457,17 +245,13 @@ export default {
     this.refactorRegistry()
     this.calcRegistryBeginEndTime()
     this.calcChartRange()
-    console.log(registryData)
-    this.chartData = [
-          ['President' , 'start', 'end']
-      ]
 
     for (var i in this.registryData) {
       this.chartData.push([ this.registryData[i].RegistryName, new Date(this.registryData[i].BeginTime), new Date(this.registryData[i].EndTime) ])
     }
 
-
-
+    console.log(this.chartData)
+    console.log(this.chartOptions.hAxis.minValue.getHours())
   }
 }
 </script>
